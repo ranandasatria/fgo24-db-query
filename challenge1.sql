975c6d228d751ccf0a43c1cd091c1dba71593898
ranandasatria <rananda24@gmail.com>
2025-06-17 10:04:16 +0700

    chore: query for task 3

--- challenge1.sql
+++ challenge1.sql
@@ -1,6 +1,10 @@
--- 1. cari movie dengan tahun rilis lebih dari 2000
+-- 1. mencari movie dengan tahun rilis lebih dari 2000
 -- 2. mencari aktor dengan akhiran nama 's'
+-- 3. mencari movie dengan rating diantara 5 dan 7 dan tahun rilis 2004 sampai 2006
 
 SELECT * FROM movies WHERE year > 2000;
 
 SELECT * FROM actors WHERE last_name ilike '%s';
+
+SELECT * FROM movies WHERE rankscore BETWEEN 5 AND 7 AND year BETWEEN 2004 AND 2006 ORDER BY rankscore DESC;
+
