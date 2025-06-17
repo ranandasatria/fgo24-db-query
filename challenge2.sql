743f664ed8ad3170d60d3c8dbdcf9c7fc3b1217b
ranandasatria <rananda24@gmail.com>
2025-06-17 11:11:47 +0700

    chore: show only 1 genre and 1 director

--- challenge2.sql
+++ challenge2.sql
@@ -6,3 +6,11 @@ JOIN movies_directors md on md.movie_id = m.id
 JOIN directors d on  d.id = md.director_id
 JOIN movies_genres mg on mg.movie_id = m.id
 LIMIT 50;
+
+-- jika hanya ingin menampilkan 1 director dan 1 genre
+SELECT DISTINCT ON (m.id) m.id, m.name AS title, m.year, m.rankscore AS rating, d.first_name AS director_first_name, d.last_name AS director_last_name, mg.genre FROM movies m
+JOIN movies_directors md ON md.movie_id = m.id
+JOIN directors d ON d.id = md.director_id
+JOIN movies_genres mg ON mg.movie_id = m.id
+ORDER BY m.id, mg.genre
+LIMIT 50;
