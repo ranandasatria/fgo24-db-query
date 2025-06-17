c7394bf145cc1bdaf01f83dcbc5ade47454d89c1
ranandasatria <rananda24@gmail.com>
2025-06-17 10:55:49 +0700

    chore: query for challenge 2

---
+++ challenge2.sql
@@ -0,0 +1,8 @@
+--4. Melakukan join director, genre ke table movies, 
+-- dapatkan hanya 5 movie pertama saja
+
+SELECT m.id, m.name as title, m.year, m.rankscore as rating, d.first_name as director_first_name, d.last_name as director_last_name, mg.genre FROM movies m
+JOIN movies_directors md on md.movie_id = m.id
+JOIN directors d on  d.id = m.id
+JOIN movies_genres mg on mg.movie_id = m.id
+LIMIT 5
