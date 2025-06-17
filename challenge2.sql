93e278f57c85f3f909de2f48ddf781774899a918
ranandasatria <rananda24@gmail.com>
2025-06-17 11:01:36 +0700

    chore: query for challenge 2

--- challenge2.sql
+++ challenge2.sql
@@ -3,6 +3,6 @@
 
 SELECT m.id, m.name as title, m.year, m.rankscore as rating, d.first_name as director_first_name, d.last_name as director_last_name, mg.genre FROM movies m
 JOIN movies_directors md on md.movie_id = m.id
-JOIN directors d on  d.id = m.id
+JOIN directors d on  d.id = md.director_id
 JOIN movies_genres mg on mg.movie_id = m.id
-LIMIT 50
+LIMIT 50;
