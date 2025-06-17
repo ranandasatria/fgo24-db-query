93661edc0d285ee86995a65c589b5c2bf2512a56
ranandasatria <rananda24@gmail.com>
2025-06-17 09:55:32 +0700

    chore: query for filtering year and actor name

--- challenge1.sql
+++ challenge1.sql
@@ -1,6 +1,6 @@
 -- 1. cari movie dengan tahun rilis lebih dari 2000
 -- 2. mencari aktor dengan akhiran nama 's'
 
-SELECT * FROM movies WHERE year > 2001;
+SELECT * FROM movies WHERE year > 2000;
 
 SELECT * FROM actors WHERE last_name ilike '%s';
